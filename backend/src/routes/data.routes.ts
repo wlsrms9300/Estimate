26249ffcca2b94da2f59409cf2c0ad20fe3abab4
Jingeun <wlsrms9300@naver.com>
2025-03-20 16:39:04 +0900

    route add '/'

--- backend/src/routes/data.routes.ts
+++ backend/src/routes/data.routes.ts
@@ -3,6 +3,7 @@ import { dataController } from '../controllers/data.controller';
 
 const router = express.Router();
 
+router.get('/', function (req, res) { res.send('테스트입니다.'); });
 router.get('/members', dataController.getMembers);
 
 export default router; 
