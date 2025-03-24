a0abc8546988ff7993e68382be68318b515f8656
Jingeun <wlsrms9300@naver.com>
2025-03-25 03:17:02 +0900

    mainLayout UI edit

--- frontend/src/components/layout/Navigation.tsx
+++ frontend/src/components/layout/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { Layout } from 'antd'
-import { FileTextOutlined, SettingOutlined, UserOutlined, BellOutlined } from '@ant-design/icons'
+import { Layout, Button, Tooltip, Flex } from 'antd'
+import { FormOutlined, SettingOutlined, UserOutlined, BellOutlined, MenuOutlined } from '@ant-design/icons'
 import { soMainStyles } from '../../styles/somain.styles'
 import { useNavigate } from 'react-router-dom'
 
@@ -13,12 +13,23 @@ export default function Navigation() {
     return (
         <Sider width={240} collapsed={siderCollapsed} className={soMainStyles.sider} style={{ background: 'white' }}>
             <div className={soMainStyles.siderHeader}>
-                <div className={soMainStyles.siderLogo}>BizSync</div>
+                {siderCollapsed ? '' : <div className={soMainStyles.siderLogo}>BizSync</div>}
+                <Tooltip title={siderCollapsed ? '메뉴 열기' : '메뉴 닫기'} placement="right">
+                    <Button
+                        onClick={() => setSiderCollapsed(!siderCollapsed)}
+                        shape="circle"
+                        icon={<MenuOutlined />}
+                        className={soMainStyles.siderCollapseButton}
+                    />
+                </Tooltip>
             </div>
             <div className={soMainStyles.siderMenu}>
-                <div className={soMainStyles.siderMenuItem} onClick={() => navigate('/so/estimate')}>
-                    작성하기
-                </div>
+                <Button type="primary" onClick={() => navigate('/so/estimate')} className={soMainStyles.siderCreateButton}>
+                    <Flex align="center" justify="center" gap={6}>
+                        <FormOutlined className="text-lg mr-0" />
+                        {siderCollapsed ? '' : <span>견적서 작성하기</span>}
+                    </Flex>
+                </Button>
                 <div className={soMainStyles.siderMenuItem}>서류함</div>
                 <div className={soMainStyles.siderMenuItem}>요금제</div>
                 <div className={soMainStyles.siderMenuItem}>업체 관리</div>
